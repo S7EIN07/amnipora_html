b3bbee09b6344838578c020e11ca10550715a22c
Jailson Stein <steinjailson@gmail.com>
2025-06-23 11:54:38 -0300

    commit site splash

---
+++ main.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    document.getElementById('splash').style.display = 'none';
+    document.getElementById('main-content').style.display = 'block';
+  }, 2000);
+});
