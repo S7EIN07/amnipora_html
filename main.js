7ae5cfb41249aa450b9e56f2723cb36879b729fc
Jailson Stein <steinjailson@gmail.com>
2025-06-25 16:40:05 -0300

    ahn

--- main.js
+++ main.js
@@ -80,6 +80,13 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: 'slow' })
-  ;
+  const scrollDuration = 500;
+  const scrollStep = -window.scrollY / (scrollDuration / 15);
+  const scrollInterval = setInterval(() => {
+    if (window.scrollY !== 0) {
+      window.scrollBy(0, scrollStep);
+    } else {
+      clearInterval(scrollInterval);
+    }
+  }, 15);
 }
